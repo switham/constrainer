f3648851a08c028ea30b8fb8849fc6d3fdcb939e
Steve Witham <sw@tiac.net>
2011-05-24 00:20:07 -0400

    spell_dice += classes State, StateRowOrCol; --verbose, stats and phrases.
    Most state info kept in state; most functions moved out to the top level.
    Trues and Maybes in rows and columns are recounted not incrementally kept.
    Still calls the (renamed) report_solution() function at the bottom of
    the recursion rather than being a generator.  Added --verbose option.
    
    spell_dice_stats is a short script that counts verbose outputs by depth.
    
    spell_dice_phrases is just a list of phrases I've tried.

--- spell_dice.py
+++ spell_dice.py
@@ -20,6 +20,9 @@ def parse_args():
     parser.add_argument("--count", "-c",
         action="store_true",
         help="just output a count of the number of solutions found")
+    parser.add_argument("--verbose", "-v",
+        action="store_true",
+        help="show search progress")
     parser.add_argument("word",
         type=str, help="word to spell out")
     return parser.parse_args()
@@ -49,23 +52,22 @@ Maybe = Maybies()
 
 
 class State(list):
-    def __init__(self):
+    def __init__(self, iterator):
+        for row in iterator:
+            self.append(row)
         self.log_stack = []
-        self.listeners = [[list() for col in self[0]] for row in self]
-
-    def _inform_listeners_and_set(self, row, col, new_y):
-        old_y = self[row][col]
-        for listener in self.listeners[row][col]:
-            listener(old_y, new_y)
-        self[row][col] = new_y
+        self.rows = []
+        self.cols = []
 
     def set(self, row, col, new_y):
-        self._inform_listeners_and_set(row, col, new_y)
-        self.log_stack[-1].append(row, col, old_y)
+        """ Setting doesn't adjust the rows' and columns' counts. """
+        self.log_stack[-1].append((row, col, self[row][col]))
+        self[row][col] = new_y
 
     def rewind(self):
-        for row, col, popped_y in reversed(pop(self.log_stack)):
-            self._inform_listeners_and_set(row, col, popped_y)
+        """ Rewinding doesn't adjust the rows' and columns' counts. """
+        for row, col, popped_y in reversed(self.log_stack.pop()):
+            self[row][col] = popped_y
         
 
 class StateRowOrCol(object):
@@ -76,18 +78,21 @@ class StateRowOrCol(object):
         self.min_True, self.max_True = min_True, max_True
         #
         self.len = len(state) * dr + len(state[0]) * dc
-        self._n_True = sum(x == True for x in self)
-        self._n_Maybe = sum(x == Maybe for x in self)
-        for i in range(len(self)):
-            r, c = self.row_col(i)
-            self.state.listeners[row][col].append(self._hear)
+        self.recount()
 
     def __len__(self):
         return self.len
 
+    def recount(self):
+        self._n_True = sum(x == True for x in self)
+        self._n_Maybe = sum(x == Maybe for x in self)
+
     def n_True(self): return self._n_True
 
     def n_Maybe(self): return self._n_Maybe
+
+    def freedom(self):
+        return self.n_Maybe() + self.n_True() - self.min_True
 
     def row_col(self, i):
         return self.r0 + i * self.dr, self.c0 + i * self.dc
@@ -96,133 +101,125 @@ class StateRowOrCol(object):
         r, c = self.row_col(x)
         return self.state[r][c]
         
-    def __iter__(self):
-        for i in range(len(self)):
-            yield self[i]
-
     def __setitem__(self, x, new_y):
         r, c = self.row_col(x)
         self.state.set(r, c, new_y)
 
-    def _hear(self, old_y, new_y):
-        self.n_True += -(old_y == True) + (new_y == True)
-        self.n_Maybe += -(old_y == Maybe) + (new_y == Maybe)
+    def __iter__(self):
+        for i in range(len(self)):
+            # print "r0=%d, dr=%d,  c0=%d, dc=%d,  i=%d" % (self.r0, self.dr, self.c0, self.dc, i)
+            yield self[i]
 
 
-def spell(word, dice, multi=False, just_count=False):
+class Done(Exception):
+    pass
+
+
+def spell(word, dice, multi=False, just_count=False, verbose=False):
     letters = list(set(word))
-    state = [[(letter in die.faces and Maybe) for letter in letters] for die in dice]
-    log_stack = []
-    letter_cols = []
+    state = State([(letter in die.faces and Maybe) for letter in letters] for die in dice)
+    state.cols = []
     for j, letter in enumerate(letters):
-        min_True = max_True = [sum(c == letter for c in word)]
-        letter_cols.append(StateRowOrCol(state, log_stack, 0, j, 0, 1, min_True, max_True))
-    dice_rows = []
+        min_True = max_True = sum(c == letter for c in word)
+        state.cols.append(StateRowOrCol(state, 0, j, 1, 0, min_True, max_True))
     if len(dice) == len(word):
         min_True, max_True = 1, 1
     else:
         min_True, max_True = 0, 1
+    state.rows = []
     for i, die in enumerate(dice):
-        dice_rows.append(StateRowOrCol(state, log_stack, i, 0, 1, 0, min_True, max_True))
-    spell.n_solutions = 0
+        state.rows.append(StateRowOrCol(state, i, 0, 0, 1, min_True, max_True))
+    state.n_solutions = 0
     
-    class Done(Exception):
-        pass
-
-    class Stuck(Exception):
-        pass
-
-    def solution():
-        spell.n_solutions += 1
+    def report_solution():
+        state.n_solutions += 1
+        if verbose: print "===== solution", state.n_solutions, len(state.log_stack), "====="
         if just_count:
             return
-        
-        letter_dice = collections.defaultdict(list)
-        for col in cols:
-            letter = letters[col]
-            for row in rows:
-                if state[row][col] == True:
-                    letter_dice[letter].append(dice[row])
+
+        letter_dice = dict( (letter, [die for i, die in enumerate(dice) if state[i][j] == True])
+                           for j, letter in enumerate(letters))
         for letter in word:
             die = letter_dice[letter].pop()
             print letter, die
         print
-
-
-    def massage_rows_or_cols(rcs):
-        """
-        Scan rows or columns, drawing conclusions that are certain.
-        If we're stuck, raise the Stuck exception.
-        """
-        for rc in rcs:
-            if rc.n_True() + rc.n_Maybe() < rc.min_True or rc.n_True() > rc.max_True:
-                raise Stuck()
-
-            if rc.n_True() == rc.max_True and rc.n_Maybe() > 0:
-                for i in range(len(rc)):
-                    if rc[i] == Maybe:
-                        rc[i] = False
-            if rc.n_Maybe() > 0 and rc.n_True() + rc.n_Maybe() == rc.min_True:
-                for i in range(len(rc)):
-                    if rc[i] == Maybe:
-                        rc[i] = True
-
-
-    def spell_more():
-        log = []
-
-        def move(row, col, new_value):
-            log.append((row, col, state[row][col]))
-            state[row][col] = new_value
-
-        def rewind():
-            for row, col, value in reversed(log):
-                state[row][col] = value
-
-        prev_len = -1
-        while len(log) > prev_len:
-            prev_len = len(log)
-            try:
-                dice_maybes, dice_trues = massage_rows_or_cols(move, 1, 0)
-                letter_maybes, letter_trues = massage_rows_or_cols(move, 0, 1)
-            except Stuck:
-                rewind()
-                return
-
-        if sum(dice_maybes) + sum(letter_maybes) == 0:
-            solution()
-            if multi:
-                rewind()
-                return
-            else:
-                raise Done()
-
-        # Now all certain moves have been made above.  Pick the move with the least "freedom".
-
-        def freedom(row, col):
-            die_freedom = dice_maybes[row] + dice_trues[row] - dice_mins[row]
-            letter_freedom = letter_maybes[col] + letter_trues[col] - letter_mins[col]
-            return die_freedom * letter_freedom
-
-        f, row, col = min((freedom(r, c), r, c) for r in rows for c in cols
-                                                      if state[r][c] == Maybe)
-        for tf in False, True:
-            move(row, col, tf)
-            spell_more()
-        rewind()
-        return
-
+        
     try:
-        spell_more()
+        spell_more(state, multi, report_solution, verbose)
     except Done:
         pass
-    return spell.n_solutions
+    return state.n_solutions
+
+
+class Stuck(Exception):
+    pass
+
+
+def massage_rows_or_cols(rcs, verbose):
+    """
+    Scan rows or columns, drawing conclusions that are certain.
+    Does not recount the perpendicular cols/rows.
+    If we're stuck, raise the Stuck exception.
+    """
+    for rc in rcs:
+        rc.recount()
+        if rc.n_True() + rc.n_Maybe() < rc.min_True or rc.n_True() > rc.max_True:
+            raise Stuck()
+
+        if rc.n_True() == rc.max_True and rc.n_Maybe() > 0:
+            for i in range(len(rc)):
+                if rc[i] == Maybe:
+                    if verbose: print "inferred", rc.row_col(i), False
+                    rc[i] = False
+            rc.recount()
+        if rc.n_Maybe() > 0 and rc.n_True() + rc.n_Maybe() == rc.min_True:
+            for i in range(len(rc)):
+                if rc[i] == Maybe:
+                    if verbose: print "inferred", rc.row_col(i), True
+                    rc[i] = True
+            rc.recount()
+
+
+def spell_more(state, multi, report_solution_fn, verbose):
+    state.log_stack.append([])
+
+    prev_len = -1
+    while len(state.log_stack[-1]) > prev_len:
+        prev_len = len(state.log_stack[-1])
+        try:
+            massage_rows_or_cols(state.rows, verbose)
+            massage_rows_or_cols(state.cols, verbose)
+        except Stuck:
+            state.rewind()
+            return
+
+    if sum(row.n_Maybe() for row in state.rows) == 0:
+        report_solution_fn()
+        if multi:
+            state.rewind()
+            return
+        else:
+            raise Done()
+
+    # Now all certain moves have been made above.  Pick the move with the least "freedom".
+
+    f, i, j = min((row.freedom() * col.freedom(), i, j)
+                  for i, row in enumerate(state.rows) for j, col in enumerate(state.cols)
+                  if state[i][j] == Maybe)
+    for tf in False, True:
+        if verbose: print "try", i, j, tf
+        state.set(i, j, tf)
+        spell_more(state, multi, report_solution_fn, verbose)
+        if verbose: print "===== pop", len(state.log_stack), "====="
+    state.rewind()
+    return
+
 
 
 if __name__ == "__main__":
     args = parse_args()
     dice = [Die(line) for line in open(args.dice)]
-    n_solutions = spell(args.word, dice, args.many, args.count)
+    n_solutions = spell(args.word, dice, args.many, args.count, args.verbose)
     if args.count:
         print n_solutions
     if n_solutions == 0:
