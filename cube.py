527655df7782ba712eb50cde5fe9121a183458c3
Steve Witham <sw@tiac.net>
2012-04-16 20:37:10 -0400

    cube.py: seems to be solving (no print yet)

--- cube.py
+++ cube.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 """ cube.py -- The Cube puzzle. """
 
-# from maybes import *
-# from constrainer import *
+from sys import stdout, stderr, exit
+from maybies import *
+from constrainer import *
 
 from ddict import ddict
 import argparse
@@ -28,8 +29,47 @@ def parse_args():
 
 # A shape is a list of 3D points as 3-tuples.
 # Actually that's a shape in an "orientation".
-# An "orientation" is a rotation plus a translation.
+# An "orientation" is a rotation plus a translation of a piece.
 
+class Piece(object):
+    def __init__(self, label, shape):
+        self.label = label
+        self.shape = shape
+
+    def __str__(self):
+        return self.label
+
+    def __repr__(self):
+        return "Piece(%r,...)" % self.label
+
+
+class Bloxel(object):
+    """ a point within the target shape """
+    def __init__(self, point):
+        self.point = point
+
+    @staticmethod
+    def name(point):
+        return "%1X%1X%1X" % point
+
+    def __str__(self):
+        return Bloxel.name(self.point)
+
+    def __repr__(self):
+        return "Bloxel(%r)" % Bloxel.name(self.point)
+
+
+class Orientation(object):
+    def __init__(self, piece, shape):
+        self.piece = piece
+        self.shape = shape
+
+    def __str__(self):
+        return '_'.join(Bloxel.name(pt) for pt in self.shape)
+
+    def __repr__(self):
+        return "Orientation(%s, %s)" % (self.piece, str(self))
+        
 
 def read_label_shape(stream):
     shape = []
@@ -78,10 +118,11 @@ def read_label_shape(stream):
     assert len(chars) == 1, \
         "Should be only one significant character in a shape, not %s" % chars
 
-    return list(chars)[0], shape
+    return list(chars)[0], canonical_shape_copy(shape)
 
 
 def canonical_shape_copy(shape):
+    """ A shape is in canonical form when the mininums of x, y & z are zero. """
     mins = tuple(min(pt[dim] for pt in shape) for dim in range(3))
     new = (tuple(pt[dim] - mins[dim] for dim in range(3)) for pt in shape)
     return tuple(sorted(new))
@@ -130,13 +171,14 @@ def all_translations_fitting(shape, target):
     return survivors
 
 
-def all_orientations_fitting(shape, target):
+def all_orientations_fitting(piece, target):
     """
     An "orientation" is a combination of a rotation and a translation
     of a specific puzzle piece.
     """
-    return sum((all_translations_fitting(rs, target)
-                for rs in all_unique_rotations(shape)), [])
+    shapes = sum((all_translations_fitting(rs, target)
+                for rs in all_unique_rotations(piece.shape)), [])
+    return [Orientation(piece, shape) for shape in shapes]
 
 
 def range_cover(values, step=1):
@@ -216,6 +258,10 @@ def print_array_of_pics(pics, n_up=2):
     
 
 def read_labels_shapes(filename):
+    """
+    Read shapes, each with a letter label, and return a list of
+    (letter, shape).
+    """
     results = []
     with open(filename) as stream:
         while True:
@@ -240,7 +286,7 @@ def show_rotations(filenames):
 def show_first_orientations(shapes_filename, target_filename):
     target_label, target = read_labels_shapes(target_filename) [0]
     for label, shape in read_labels_shapes(shapes_filename):
-        orientations = all_orientations_fitting(shape, target)
+        orientations = all_orientations_fitting(Piece(label, shape), target)
         print label, len(orientations), "orientations"
             
 
@@ -253,71 +299,87 @@ def old_main():
     show_first_rotations()
 
 
-def solve(target, pieces, multi=False, just_count=False, verbose=False):
-    """ target is a shape.  pieces is a dict of {label_letter: shape}. """
+def solve(target, piece_shapes, multi=False, just_count=False, verbose=False):
+    """
+    target is a shape.
+    piece_shapes is a dict of {label_letter: shape}.
+    """
     # Quick summary:
     #     7 pieces, up to 27 target bloxels, up to about 700 piece-orientations.
     #     Let's only use "bloxel" to refer to points in the target.
     #     Two kinds of vars:
     #         orientation-is-used
-    #         piece-unused  ("unused" is not an orientation as it has no var)
+    #         piece-is-unused
     #     Constraint types:
     #         Each bloxel is occupied exactly once.
-    #         Exactly one orientation, or unused, per piece.
+    #         Each piece is in exactly one orientation, or unused.
     #         A certain number of pieces with four points are unused.
     #         Known ahead of time whether the 3-point "r" piece is used or not.
     # Comparison to spelling with letter-dice:
-    #     A piece is kinda like a die.
-    #     An orientation is kinda like a face of a die.
-    #     A target bloxel is kinda like a letter of the word to be spelled.
+    #     A piece is like a die, an orientation is like a die face, and 
+    #     a target bloxel is like a letter of the word to be spelled.
     #     But, each orientation occupies multiple bloxels, so a piece can
     #         potentially have alternative ways to occupy a bloxel.
     #         The useable die faces were vars at the intersection of one die
-    #         and one letter.  But each orientation is a var constrained by
-    #         one piece and by multiple bloxels.
+    #         and one letter.  But each cube piece orientation is a var
+    #         constrained by one piece and by multiple bloxels.
     state = State(verbose=verbose)
-    
-    bloxels = list(set(target))
-    for i, piece in enumerate(pieces):
-        if not any(bloxel in piece.orientations for bloxel in bloxels):
-            print "Piece", piece, "is not usable."
-            pieces.pop(i)
-    if len(pieces) < len(target):
-        raise Exception("Not enough pieces to solve the puzzle!")
 
-    # First we set up the Constrainers:
+    # First we set up the Constraints:
 
-    bloxel_constraints = {}
+    # Each bloxel is occupied exactly once.
+    point_bloxels = dict( (point, Bloxel(point)) for point in target)
+    bloxels = point_bloxels.values()
+    occupied_once = {}
     for bloxel in bloxels:
-        # There are exactly as many pieces occupying a bloxel
-        # as appearances of the bloxel in the target.
-        n = sum(c == bloxel for c in target)
-        bloxel_constraints[bloxel] = BoolConstraint(state, bloxel=bloxel,
-                                                    min_True=n, max_True=n)
-
-    # It helps here to treat unused pieces as like being
-    # "used for nothing," or "occupying the null bloxel."
-    # The number of unused pieces is exactly as many as the target doesn't need:
-    n = len(pieces) - len(target)
-    bloxel_constraints["unused"] = BoolConstraint(state, bloxel="unused",
-                                                  min_True=n, max_True=n)
+        occupied_once[bloxel] = BoolConstraint(state, bloxel=bloxel,
+                                                      min_True=1, max_True=1)
 
     # Each piece is used exactly once: to occupy a bloxel, or for nothing:
-    piece_constraints = dict( (piece, BoolConstraint(state, piece=piece,
-                                                     min_True=1, max_True=1))
-                              for piece in pieces)
+    labeled_pieces = dict( (label, Piece(label, shape))
+                           for label, shape in piece_shapes.iteritems())
+    pieces = labeled_pieces.values()
+    oriented_one_way = {}
+    for piece in pieces:
+        oriented_one_way[piece] = BoolConstraint(state, piece=piece,
+                                                        min_True=1, max_True=1)
 
-    # Now the Constrainees (variables):
-    
-    for bloxel in bloxels + ["unused"]:
-        for piece in pieces:
-            # Variables to say: this piece is used to occupy this bloxel
-            # (or, this piece is not used).
-            if bloxel == "unused" or bloxel in piece.orientations:
-                piece_occupys_bloxel = BoolVar(state, piece=piece,
-                                               bloxel=bloxel)
-                bloxel_constraints[bloxel].constrain(piece_occupys_bloxel)
-                piece_constraints[piece].constrain(piece_occupys_bloxel)
+    # Constraints on how many pieces are unused, for two sizes of piece:
+    n_unused = {}
+    n_4_pieces = sum(1 for piece in pieces if len(piece.shape) == 4)
+    n_3_pieces = sum(1 for piece in pieces if len(piece.shape) == 3)
+    assert n_3_pieces == 1 and n_3_pieces + n_4_pieces == len(pieces), \
+        "Don't know how to work with this set of pieces, sorry!"
+    n_unused[4] = n_4_pieces - (len(target) / 4)
+    n_unused[3] = n_3_pieces - (len(target) % 4) / 3
+    assert len(target) % 4 in (3, 0) and n_unused[4] >= 0, \
+        "Target has %d bloxels, can't be made out of the pieces." % len(target)
+    how_many_unused = {}
+    for piece_size in 3, 4:
+        how_many_unused[piece_size] = \
+            BoolConstraint(state, piece_size=piece_size,
+                                  min_True=n_unused[piece_size],
+                                  max_True=n_unused[piece_size])
+
+    # Now the variables:
+
+    for piece in pieces:
+        piece_unused = BoolVar(state, label="unused")
+        oriented_one_way[piece].constrain(piece_unused)
+        piece_size = len(piece.shape)
+        how_many_unused[piece_size].constrain(piece_unused)
+        
+        piece.orientations = all_orientations_fitting(piece, target)
+        for orientation in piece.orientations:
+            bloxels = [point_bloxels[pt] for pt in orientation.shape]
+            oriented_thus = BoolVar(state, orientation=orientation,
+                                           bloxels=bloxels)
+            oriented_one_way[piece].constrain(oriented_thus)
+            for bloxel in bloxels:
+                occupied_once[bloxel].constrain(oriented_thus)
+            
+
+    # Go solve it.
         
     n_solutions = 0
     n_deadends = 0
@@ -327,29 +389,16 @@ def solve(target, pieces, multi=False, just_count=False, verbose=False):
             continue
 
         n_solutions += 1
-        if verbose:
-            print "===== solution", n_solutions, state.depth(), "====="
-            sys.stdout.flush()
+        if not just_count or verbose:
+            print "==== solution", n_solutions, "depth", "%d," % state.depth(),
+            print n_deadends, "dead ends ===="
+            stdout.flush()
         if just_count:
             continue
 
         # Show a solution.
-        # For each bloxel, make a list of pieces that are occupying it.
-        bloxel_pieces = dict( (bloxel, []) for bloxel in bloxels)
-        for bloxel in bloxels:
-            for var in bloxel_constraints[bloxel].vars:
-                if var.value == Maybe:
-                    print var.bloxel, var.piece, "Maybe??"
-                    print var.bloxel, "constraints:"
-                    print [c2.value for c2 in bloxel_constraints[var.bloxel].vars]
-                    print var.piece, "constraints:"
-                    print [c2.value for c2 in piece_constraints[var.piece].vars]
-                if var.value:
-                    bloxel_pieces[bloxel].append(var.piece)
-        # Remove pieces from their lists as you use them to solve:
-        for bloxel in target:
-            piece = bloxel_pieces[bloxel].pop()
-            print bloxel, piece
+        print "I'd like to show you the solution here."
+        
         print
         if not multi:
             break
@@ -358,12 +407,9 @@ def solve(target, pieces, multi=False, just_count=False, verbose=False):
 
 
 if __name__ == "__main__":
-    old_main()
-
-elif False:
     args = parse_args()
     pieces = dict(read_labels_shapes(args.pieces))
-    target_label, target = read_labels_shapes(args.puzzle)
+    target_label, target = read_labels_shapes(args.puzzle) [0]
     
     n_solutions, n_deadends = solve(target, pieces,
                                     args.many, args.count, args.verbose)
@@ -371,8 +417,8 @@ elif False:
         print n_solutions, "solutions."
     if n_solutions == 0:
         if not args.count:
-            print >>sys.stderr, "No solutions."
+            print >>stderr, "No solutions."
     print n_deadends, "dead ends"
     if n_solutions == 0:
-        sys.exit(1)
+        exit(1)
 
